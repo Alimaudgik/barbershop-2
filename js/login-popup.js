3076483905303bdd5667f2a6c6d4dab949c5bc48
Arseni <arseni_p@tut.by>
2019-05-30 17:08:50 +0300

    popup

---
+++ js/login-popup.js
@@ -0,0 +1,31 @@
+var popup = document.querySelector('.login-page');
+var link = document.querySelector('.user-list__login');
+var close = document.querySelector('.button-close')
+
+link.addEventListener('click', function() {
+  if (popup.classList.contains('login-page__closed')) {
+    popup.classList.remove('login-page__closed');
+    popup.classList.add('login-page__opened');
+  } else {
+    popup.classList.add('login-page__closed');
+    popup.classList.remove('login-page__opened');
+  }
+});
+
+close.addEventListener('click', function() {
+  evt.preventDefault();
+    popup.classList.add('login-page__closed');
+    popup.classList.remove('login-page__opened');
+  });
+
+window.addEventListener('keydown', function() {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('login-page__opened')) {
+      popup.classList.remove('login-page__opened');
+      popup.classList.add('login-page__closed');
+    }
+  }
+});
+
+
+
